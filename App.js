ef52ad6115ed8b8a80bc2133ba23bbcce9a54ac0
Vishesh Singh <VisheshSingh16@gmail.com>
2020-03-16 23:13:30 -0500

    Stack Navigator 📚

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
-import Home from './screens/home';
+import Navigator from './routes/homeStack';
 import { AppLoading, SplashScreen } from 'expo';
 
 import { YellowBox } from 'react-native';
@@ -17,7 +17,7 @@ export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   if (fontsLoaded) {
-    return <Home />;
+    return <Navigator />;
   } else {
     return (
       <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
